400d48b5783dccac653e84acfbf9e1f36d275c37
emil151997 <emil151997@mail.ru>
2020-05-23 23:30:22 +0300

    Update Dispatcher implementation

--- src/Dispatcher.py
+++ src/Dispatcher.py
@@ -1,8 +1,187 @@
 import Camera
 import Server
+import Record
+import psycopg2
 
-ser = Server.Server("3", "24.24.22.22", "ASD12", "Cisco", "2020-10-21 08:00:31",
-                    "2020-11-21 11:31:51", "New Server")
+conn = psycopg2.connect(dbname='postgres', user='postgres',
+                        password='123', host='localhost')
 
-cam = Camera.Camera("3", "3", "24.24.22.22", "New-York,33-th Avenue", "ASD12", "Cisco", "2020-10-21 08:00:31",
-                    "2020-11-21 11:31:51", "New Camera")
+
+def findAllServers():
+    try:
+        cur = conn.cursor()
+        # execute the INSERT statement
+        sql = "SELECT * FROM \"Server\""
+        cur.execute(sql)
+        # commit the changes to the database
+        conn.commit()
+        for row in cur:
+            print(row)
+        count = cur.rowcount
+        print(count, "findAllServers success")
+
+    except (Exception, psycopg2.Error) as error:
+        if conn:
+            print("findAllServers failed", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
+
+def findAllCameras():
+    try:
+        cur = conn.cursor()
+        # execute the INSERT statement
+        sql = "SELECT * FROM \"Camera\""
+        cur.execute(sql)
+        # commit the changes to the database
+        conn.commit()
+        for row in cur:
+            print(row)
+        count = cur.rowcount
+        print(count, "findAllCameras success")
+
+    except (Exception, psycopg2.Error) as error:
+        if conn:
+            print("findAllCameras failed", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
+
+def findAllPairs():
+    try:
+        cur = conn.cursor()
+        # execute the INSERT statement
+        sql = "SELECT * FROM \"Camera\" WHERE server_id>0"
+        cur.execute(sql)
+        # commit the changes to the database
+        conn.commit()
+        for row in cur:
+            print(row)
+        count = cur.rowcount
+        print(count, "findAllPairs success")
+
+    except (Exception, psycopg2.Error) as error:
+        if conn:
+            print("findAllPairs failed", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
+
+def ServerByCamID(camera_id):
+    try:
+        cur = conn.cursor()
+        # execute the INSERT statement
+        sql = "SELECT * FROM \"Server\" JOIN \"Camera\" on  \"Camera.server_id=Server.server_id\" WHERE " \
+              "\"Camera.camera_id=%s\">0"
+        cur.execute(sql, camera_id)
+        # commit the changes to the database
+        conn.commit()
+        for row in cur:
+            print(row)
+        count = cur.rowcount
+        print(count, "ServerByCamID success")
+
+    except (Exception, psycopg2.Error) as error:
+        if conn:
+            print("ServerByCamID failed", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
+
+def findCamerasByServerId(server_id):
+    try:
+        cur = conn.cursor()
+        # execute the INSERT statement
+        sql = "SELECT * FROM \"Camera\" JOIN \"Server\" on  \"Server.server_id=Camera.server_id\" WHERE " \
+              "\"Camera.server_id=%s\">0"
+        cur.execute(sql, server_id)
+        # commit the changes to the database
+        conn.commit()
+        for row in cur:
+            print(row)
+        count = cur.rowcount
+        print(count, "findCamerasByServerId success")
+
+    except (Exception, psycopg2.Error) as error:
+        if conn:
+            print("findCamerasByServerId failed", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
+
+def runRoundRobin():
+    """
+    runRoundRobin implementation
+    :return:
+    """
+
+
+def bindCameraWithServer(camera_id,server_id):
+    try:
+        cur = conn.cursor()
+        # execute the INSERT statement
+        sql = "UPDATE \"Camera\" SET 'server_id' = %s WHERE 'camera_id' = '%s'"
+        cur.execute(sql, server_id, camera_id)
+        # commit the changes to the database
+        conn.commit()
+        count = cur.rowcount
+        print(count, "bindCameraWithServer success")
+
+    except (Exception, psycopg2.Error) as error:
+        if conn:
+            print("bindCameraWithServer failed", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
+
+def unbindCamera(camera_id):
+    try:
+        cur = conn.cursor()
+        # execute the INSERT statement
+        sql = "UPDATE \"Camera\" SET 'server_id' = 0 WHERE 'camera_id' = '%s'"
+        cur.execute(sql, camera_id)
+        # commit the changes to the database
+        conn.commit()
+        count = cur.rowcount
+        print(count, "unbindCamera success")
+
+    except (Exception, psycopg2.Error) as error:
+        if conn:
+            print("unbindCamera failed", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
